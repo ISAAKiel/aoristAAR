15885d1540681f3778f029aa5ddf1fc3dfb3f979
Clemens Schmid <clemens@nevrome.de>
2019-03-01 15:52:00 +0100

    reworked setup for aorist function

--- R/aorist.R
+++ R/aorist.R
@@ -1,47 +1,191 @@
 #' Calculates an aoristic sum based on a start and end date
 #'
-#' Calculates an aoristic sum based on a start and end date. Dates BC(E) have to be given with negative sign.
+#' Calculates an aoristic sum based on a start and end date. Dates BC(E) have to be given with
+#' negative sign.
 #'
-#' @param from vector of start dates
-#' @param to vector of end dates
-#' @param correct apply a correction for overlapping phases (see details)
+#' @param x s
+#' @param from s
+#' @param to s
+#' @param split_vars s
+#' @param stepwidth s
+#' @param method s
 #'
 #' @return a vector containing the aoristic sum for the entered data
 #'
-#' @details According to Mischka (2004), Aoristic analysis 'is a method used in criminology to analyse crime incidents and determine probabilities for the contemporaneity of the incidents or, when applied to archaeology, for the contemporaneity of sites'.
+#' @details According to Mischka (2004), Aoristic analysis 'is a method used in criminology to
+#' analyse crime incidents and determine probabilities for the contemporaneity of the incidents
+#' or, when applied to archaeology, for the contemporaneity of sites'.
 #'
-#' The aoristic calculation distributes the probability of an event (the event has taken place at all = 1) to (time) sections of the total range within which the event may have taken place. The length of these periods can be arbitrarily chosen, for archaeological applications we set them at the annual level. This scaling can easily be brought to the desired scale by aggregation.
+#' The aoristic calculation distributes the probability of an event (the event has taken place
+#' at all = 1) to (time) sections of the total range within which the event may have taken place.
+#' The length of these periods can be arbitrarily chosen, for archaeological applications we set
+#' them at the annual level. This scaling can easily be brought to the desired scale by aggregation.
 #'
-#' The aoristic sum is then the expected value for the number of events to be assumed within this period. This can be, for example, the expected number of settlements that have been included in the calculation as individual data with start and end date as parameters.
+#' The aoristic sum is then the expected value for the number of events to be assumed within this
+#' period. This can be, for example, the expected number of settlements that have been included
+#' in the calculation as individual data with start and end date as parameters.
 #'
-#' The calculation of the aoristic sum is based on exclusive time intervals in its original implementation (Radcliffe 2000). In archaeological applications, however, overlapping time intervals often result from different dating accuracy. For example, individual sites may only be categorized as part of the Neolithic, others may be narrowed down to the Middle Neolithic Ia. The structure of the overlapping time intervals can lead to biases of the aoristic sum (Hinz/Müller-Scheeßel forthcoming), which is corrected by the algorithm by weighting down multiple time periods.
+#' The calculation of the aoristic sum is based on exclusive time intervals in its original
+#' implementation (Radcliffe 2000). In archaeological applications, however, overlapping time
+#' intervals often result from different dating accuracy. For example, individual sites may only
+#' be categorized as part of the Neolithic, others may be narrowed down to the Middle Neolithic Ia.
+#' The structure of the overlapping time intervals can lead to biases of the aoristic sum
+#' (Hinz/Müller-Scheeßel forthcoming), which is corrected by the algorithm by weighting down
+#' multiple time periods.
 #'
 #' @references {
-#'
 #'   \insertRef{ratcliffe_aoristic_2000}{aoristAAR}
-#'
 #'   \insertRef{mischka_aoristische_2004}{aoristAAR}
-#'
 #'   \insertRef{hinz_systematic_nodate}{aoristAAR}
 #' }
 #'
 #' @examples
-#' my_settlements <- data.frame(from = c(-3800, -3750, -3500),
-#'                              to   = c(-3700, -3400, -3300))
+#' # creating test data
+#' my_settlements <- data.frame(
+#'   from = c(-3800, -3750, -3500, -4000, -3800, -3800, -3550, -3750, -3800),
+#'   to   = c(-3700, -3400, -3300, -3300, -3500, -3300, -3525, -3650, -3700),
+#'   type = c("hillfort", "hillfort", "hillfort", "hillfort", "hillfort",
+#'   "coastal settlement", "coastal settlement", "coastal settlement", "coastal settlement"),
+#'   size = c(">2ha", "<2ha", ">2ha", ">2ha", "<2ha", ">2ha", "<2ha", ">2ha", "<2ha")
+#' )
 #'
-#' my_aoristic_sum <- aorist(from = my_settlements$from,
-#'                           to   = my_settlements$to)
+#' # counting number of occurences
+#' method_number_time_series <- aorist(my_settlements, method = "number")
+#' plot(method_number_time_series, type = "l")
 #'
-#' plot(my_aoristic_sum,
-#'      type="l",
-#'      xlim = c(-4100, -3200),
-#'      ylim = c(0,0.02))
+#' # normalisation methods
+#' method_weight_time_series <- aorist(my_settlements, method = "weight")
+#' method_period_correction_time_series <- aorist(my_settlements, method = "period_correction")
+#' plot(method_weight_time_series, type = "l", col = "blue", xlim = c(-4100, -3200))
+#' lines(method_period_correction_time_series, type = "l", col = "red", lty = 2)
+#' legend(-4100, 0.05, legend=c("weight", "period_correction"), col=c("blue", "red"), lty = 1:2, cex = 0.8)
+#'
+#' # splitting time series by additional variables
+#' splitted_time_series <- aorist(my_settlements, split_vars = c("type"), method = "weight")
+#' coastel_settlements <- subset(splitted_time_series, splitted_time_series$type == "coastal settlement")[,c(1,2)]
+#' plot(coastel_settlements, type = "l", col = "darkgreen", xlim = c(-4100, -3200))
+#' hillforts <- subset(splitted_time_series, splitted_time_series$type == "hillfort")[,c(1,2)]
+#' lines(method_period_correction_time_series, type = "l", col = "orange", lty = 2)
+#' legend(-4100, 0.04, legend=c("coastel s.", "hillforts"), col=c("darkgreen", "orange"), lty = 1, cex = 0.8)
 #'
 #' @importFrom Rdpack reprompt
 #'
 #' @export
+aorist <- function(
+  x,
+  from = "from",
+  to = "to",
+  split_vars = c(),
+  stepwidth = 1,
+  method = "number"
+) {
 
-aorist <- function(from, to, correct=F) {
+  if (length(split_vars) > 0) {
+
+    x_split <- base::split(x, lapply(split_vars, function(y) { x[[y]] }))
+    x_split_exist <- x_split[sapply(x_split, function(x) { nrow(x) > 0})]
+    x_list <- x_split_exist
+
+    artefact_timeseries <- pbapply::pblapply(
+      names(x_list),
+      function(nx, split_vars, stepwidth, method) {
+        nox <- x_list[[nx]]
+        timeseries <- seq2ts(
+          nox[[from]],
+          nox[[to]],
+          stepwidth = stepwidth,
+          method = method
+        )
+        for (i in split_vars) { timeseries[[i]] <- nox[[i]][1] }
+        return(timeseries)
+      },
+      split_vars = split_vars,
+      stepwidth = stepwidth,
+      method = method
+    )
+
+    artefact_timeseries_df <- do.call(rbind, artefact_timeseries)
+
+  } else {
+
+    artefact_timeseries_df <- seq2ts(
+      x[[from]],
+      x[[to]],
+      stepwidth = stepwidth,
+      method = method
+    )
+
+  }
+
+  return(artefact_timeseries_df)
+
+}
+
+#### method switch ####
+
+seq2ts <- function(from, to, stepwidth = 1, method = "number") {
+
+  if (all(is.na(from)) | all(is.na(to))) {
+    return(tibble::tibble(date = NA_real_, sum = NA_real_))
+  }
+
+  return(
+    switch (method,
+      "number" = method_number(from, to, stepwidth = stepwidth),
+      "weight" = method_weight(from, to, stepwidth = stepwidth),
+      "period_correction" = method_period_correction(from, to, stepwidth = stepwidth)
+    )
+  )
+
+}
+
+#### methods ####
+
+# simple counting of occurences
+method_number <- function(from, to, stepwidth = 1) {
+  input <- tibble::tibble(from, to)
+  input$number_of_years <- abs(input$from - input$to)
+  input$number_of_years <- ifelse(input$number_of_years == 0, 1, input$number_of_years)
+
+    output <- tibble::tibble(date = seq(min(from, na.rm = T), max(to, na.rm = T), by = stepwidth))
+  output$sum <- sapply(
+    output$date,
+    function(y, x) {
+      nrow(subset(x, x$from <= y & x$to >= y))
+    },
+    input
+  )
+  output$sum[output$sum == 0] <- NA_real_
+
+  return(output)
+}
+
+# weighting by dating precision
+method_weight <- function(from, to, stepwidth = 1) {
+  input <- tibble::tibble(from, to)
+  input$number_of_years <- abs(input$from - input$to)
+  input$number_of_years <- ifelse(input$number_of_years == 0, 1, input$number_of_years)
+  input$weight_per_year = 1/input$number_of_years
+
+  output <- tibble::tibble(date = seq(min(from, na.rm = T), max(to, na.rm = T), by = stepwidth))
+  output$sum <- sapply(
+    output$date,
+    function(y, x) {
+      sum(subset(x, x$from <= y & x$to >= y)$weight_per_year)
+    },
+    input
+  )
+  output$sum[output$sum == 0] <- NA_real_
+
+  return(output)
+}
+
+# weighting by period attribution
+method_period_correction <- function(from, to, stepwidth = 1) {
+
+  if (stepwidth != 1) {
+    stop("Method 'perdiod_correction' only works with stepwidth = 1.")
+  }
 
   dates <- as.data.frame(cbind(from,to))
   unique_periodes <- unique(dates)
@@ -66,7 +210,7 @@ aorist <- function(from, to, correct=F) {
     ao_weight[i,]<-rep(0,nrow(n_periods))
     within_period <- n_periods$date>=this_period$from & n_periods$date<=this_period$to
     if (correct) {
-    ao_weight[i,within_period]<-1/n_periods$sum[within_period]
+      ao_weight[i,within_period]<-1/n_periods$sum[within_period]
     } else {
       ao_weight[i,within_period]<-1
     }
